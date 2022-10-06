9f9e178cfad1412b9c0bc322805a06b65f1c73ce
Alexander Kuvaev <vinatorul@yandex-team.ru>
2022-10-06 13:34:42 +0300

    Add VIP tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,20 +1,17 @@
 import pytest
 
-from testsuite.databases import pgsql
-
-
 # Start the tests via `make test-debug` or `make test-release`
 
-
 async def test_basic(service_client):
-    response = await service_client.post('/v1/make-shorter', data='{"url": "http://example.com?id=123"}')
+    data = {"url": "http://example.com?id=123"}
+    response = await service_client.post('/v1/make-shorter', json=data)
     assert response.status == 200
     
     response_json = response.json()
     assert 'short_url' in response_json
 
-    request_uri = response_json['short_url']
-    request_uri = request_uri[request_uri.rfind('/'):]
+    request_url = response_json['short_url']
+    request_url = request_url[request_url.rfind('/'):]
 
-    response = await service_client.get(request_uri)
+    response = await service_client.get(request_url)
     assert response.status == 200
